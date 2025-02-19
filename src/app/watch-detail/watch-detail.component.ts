0cf146e6bc1a8271bc132576054a3ff27ddf1823
bentoumya <bentoumya.noureddine@gmail.com>
2025-02-19 23:22:07 +0100

    ajouter Body et footer et header et login et watch-detail

---
+++ src/app/watch-detail/watch-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-watch-detail',
+  templateUrl: './watch-detail.component.html',
+  styleUrls: ['./watch-detail.component.css']
+})
+export class WatchDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
